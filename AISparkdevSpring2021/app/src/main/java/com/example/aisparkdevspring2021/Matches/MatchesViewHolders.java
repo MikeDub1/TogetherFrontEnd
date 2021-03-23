a077c9d770cfb82c36a5dfcb4c8c31f7bec51c24
garciamilord <68765141+garciamilord@users.noreply.github.com>
2021-03-22 23:03:43 -0400

    testOut task 12
    Former-commit-id: e421d123adb3a2074c59c7d7267c56343a5888d3 [formerly 093d318e4e28e538c620efe38ec4f7c2932c1ad1] [formerly 8e06866dddc8b1897174b7f5681918a422cc4f6b]
    Former-commit-id: 45199530f2faf5229041dce51d8e1d18247ed8cc
    Former-commit-id: 7fa7e4a48e65aa60d5907751d7a31481fcd5027c

---
+++ AISparkdevSpring2021/app/src/main/java/com/example/aisparkdevspring2021/Matches/MatchesViewHolders.java
@@ -0,0 +1,27 @@
+package com.example.aisparkdevspring2021.Matches;
+
+import android.view.View;
+import android.widget.TextView;
+import com.example.aisparkdevspring2021.R;
+
+
+import androidx.recyclerview.widget.RecyclerView;
+
+import org.w3c.dom.Text;
+
+public class MatchesViewHolders extends RecyclerView.ViewHolder implements View.OnClickListener {
+    public TextView mMatchId;
+
+
+    public MatchesViewHolders(View itemView) {
+        super(itemView);
+        itemView.setOnClickListener(this);
+
+        mMatchId=(TextView) itemView.findViewById(R.id.Matchid);
+    }
+
+    @Override
+    public void onClick(View v) {
+
+    }
+}
