c4fa925079bf8ea29d85dc2b3975e601148be73e
JeffreyMunro <79286811+JeffreyMunro@users.noreply.github.com>
2021-03-31 20:13:58 -0400

    test
    update
    
    
    Former-commit-id: c2466d7430e3327361f1c004776f46ab2a90ebdd [formerly 4ab3de9c16f1e2f696d10d5dcf7ca1450aae96a4] [formerly caefeed79857e66804e55b335a61c9bbb5072aca]
    Former-commit-id: e96985454b75e9d9f56d63ce15801421d7ee8a6b
    Former-commit-id: 4a8c5ee8583a135ed8abe66426281aade447ed53

--- AISparkdevSpring2021/app/src/main/java/com/example/aisparkdevspring2021/Matches/MatchesViewHolders.java
+++ AISparkdevSpring2021/app/src/main/java/com/example/aisparkdevspring2021/Matches/MatchesViewHolders.java
@@ -1,6 +1,7 @@
 package com.example.aisparkdevspring2021.Matches;
 
 import android.view.View;
+import android.widget.ImageView;
 import android.widget.TextView;
 import com.example.aisparkdevspring2021.R;
 
@@ -10,14 +11,16 @@ import androidx.recyclerview.widget.RecyclerView;
 import org.w3c.dom.Text;
 
 public class MatchesViewHolders extends RecyclerView.ViewHolder implements View.OnClickListener {
-    public TextView mMatchId;
-
-
+    public TextView mMatchId, mMatchName;
+    public ImageView mMatchImage;
     public MatchesViewHolders(View itemView) {
         super(itemView);
         itemView.setOnClickListener(this);
 
         mMatchId=(TextView) itemView.findViewById(R.id.Matchid);
+        mMatchName=(TextView) itemView.findViewById(R.id.MatchName);
+        mMatchImage =(ImageView) itemView.findViewById(R.id.MatchImage);
+
     }
 
     @Override
